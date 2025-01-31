b4e13b7ceea2a637956beccdddedda9ea98df216
Pau Martinez <Zenit.Uap@gmail.com>
2025-01-31 22:36:43 +0100

    Add player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -10,11 +11,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updateable.update(dt)
+
         screen.fill(pygame.Color(0, 0, 0))
+        for item in drawable:
+            item.draws(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
